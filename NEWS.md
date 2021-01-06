84b8694ed907cbf34642d1264ade99287c70eb27
Simon Zimmermann <zimmersi@hu-berlin.de>
2021-01-06 13:40:02 +0000

    version 0.1.3

--- NEWS.md
+++ NEWS.md
@@ -5,3 +5,11 @@
 # AnchorRegression 0.1.1
 
 * Initial implementation of anchor_stability with naive and post-selection decision
+
+# AnchorRegression 0.1.2
+
+* Initial implementation of weighted_anchor_regression and prediction functionality
+
+# AnchorRegression 0.1.3
+
+* Initial implementation of gam based anchor regression and prediction functionality
--- R/anchor_regression.R
+++ R/anchor_regression.R
@@ -29,6 +29,11 @@
 
 anchor_regression <- function(x, anchor, gamma, target_variable, lambda='CV'){
 
+  # preliminary checks
+  if(ncol(x)<3){
+    print("unsufficient number of columns")
+  }
+
   # convert to matrix for lm
   x <- as.matrix(x)
   anchor <- as.matrix(anchor)
@@ -41,7 +46,7 @@ anchor_regression <- function(x, anchor, gamma, target_variable, lambda='CV'){
   if(lambda=='CV'){
     cv_data <- fit_const$fitted.values + fit$residuals
     indices <- 1:nrow(cv_data)
-    j <-  match( 'V2', colnames(cv_data))
+    j <-  match( target_variable, colnames(cv_data))
     fit_glmnet_lasso <- cv.glmnet(x = cv_data[indices,-c(j)],cv_data[indices,j])
     lambda_cv <- fit_glmnet_lasso$lambda.1se
   }
@@ -50,11 +55,11 @@ anchor_regression <- function(x, anchor, gamma, target_variable, lambda='CV'){
   # transform data for the Anchor Regression
   anchor_data <- fit_const$fitted.values + fit$residuals + sqrt(gamma)*(fit$fitted.values-fit_const$fitted.values)
   indices <- 1:nrow(anchor_data)
-  j <-  match( 'V2', colnames(cv_data))
+  j <-  match( target_variable, colnames(anchor_data))
   x <- anchor_data[indices,-c(j)]
   y <- anchor_data[indices,j]
   fit_glmnet_anchor <- glmnet(x = x,y = y,lambda = lambda_cv)
 
-  return_list <- list(coeff = c(as.vector(coef(fit_glmnet_anchor))), names = c('Intercept',c(colnames(anchor_data)[!colnames(anchor_data) %in% target_variable] )), x = x, y = y,lambda = lambda_cv, model = fit_glmnet_anchor )
+  return_list <- list(coeff = c(as.vector(coef(fit_glmnet_anchor))), names = c('Intercept',c(colnames(anchor_data)[!colnames(anchor_data) %in% target_variable] )), x = x, y = y,lambda = lambda_cv, model = fit_glmnet_anchor)
   return(return_list)
 }
